e014ceb128fbc2388c529a7263e956dce7f64221
Jeremias Quevedo <112958@tecnicatura.frc.utn.edu.ar>
2022-10-02 12:17:38 -0300

    functions in player component were added

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { card } from 'src/app/interfaces/card';
+import { player } from 'src/app/interfaces/player';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.css']
+  styleUrls: ['./player.component.css'],
 })
 export class PlayerComponent implements OnInit {
-
-
-  constructor() { }
-
-  ngOnInit(): void {
+  player: player;
+  croupier: player;
+  constructor() {
+    this.player = {} as player;
+    this.croupier = {} as player;
   }
 
+  ngOnInit(): void {}
+
+  askForCard(card: card, isCroupier: boolean) {
+    isCroupier
+      ? this.croupier.gameCards.push(card)
+      : this.player.gameCards.push(card);
+  }
+
+  stopPlaying() {
+    return {
+      playerCards: this.player.gameCards,
+      croupierCards: this.croupier.gameCards,
+    };
+  }
+
+  getPlayerCards() {
+    return this.player.gameCards;
+  }
+  getCroupierCards() {
+    return this.croupier.gameCards;
+  }
 }

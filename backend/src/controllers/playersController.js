b00afba09ab69e0e19e463c0ff4e9204bd3e3bfc
RickieMedina <113144@tecnicatura.frc.utn.edu.ar>
2022-10-22 01:06:10 -0300

    add API players

--- backend/src/controllers/playersController.js
+++ backend/src/controllers/playersController.js
@@ -1,15 +1,157 @@
-const bcrypt= require('bcrypt');
-const authconfig=require('../config/auth');
-const jwt= require('jsonwebtoken');
-const pool= require('../database/db');
+const bcrypt = require("bcrypt");
+const authconfig = require("../config/auth");
+const jwt = require("jsonwebtoken");
+const pool = require("../database/db");
 
 module.exports.Player = {
-    GetDeck: async function(req,res)
-    {
-      const result= await pool.query('select * from card');
-      console.log(result);
-      res.status(200).json(result)
-      
+ 
+    GetAllPlayers:async function(req,res) {
+
+      let response = {
+        error: false,
+        data: null,
+      };
+        await pool.query('SELECT id, name, email FROM players')
+             .then(players =>{
+              response.data= players
+              res.status(200).json(response);
+             })
+             .catch(error=> {
+               console.log(error)
+
+               response.error= true;
+               response.data= error;
+               res.status(500).json(response)
+             });
+
+
+    },
+    
+    GetPlayer:async function (req,res){
+
+      let response = {
+        error: false,
+        data: null,
+      };
+    
+    const { id } = req.params;
+
+    await pool.query("SELECT id, name, email FROM players WHERE id = ?", id)
+    .then(player =>{
+
+      if (player.length === 0){
+        response.error=true;
+        response.data= { msg: "Player not found" }
+        return res.status(404).json(response)
+      } 
+      else{
+        response.data={ player: player[0]}
+        res.status(200).json(response);
+      }
+     })
+     .catch(error=> {
+      console.log(error);
+      response.error = true;
+
+      res.status(500).json(response);
+     });
     },
 
+  RegisterPlayer: async function (req, res) {
+    let response = {
+      error: false,
+      data: null,
+    };
+
+    const { name, email, password } = req.body;
+    /*
+    TODO: validation errors
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() }) 
+    }*/
+
+    const newPassword = bcrypt.hashSync(
+      password,
+      Number.parseInt(authconfig.rounds)
+    );
+
+    const newUser = {
+      name: name,
+      email: email,
+      password: newPassword,
+    };
+
+    await pool.query("INSERT INTO players SET ? ", newUser)
+      .then((result) => {
+        const token = jwt.sign({ email: email, name: name }, authconfig.secret, {
+          expiresIn: authconfig.expires,
+        });
+        response.data = token;
+
+        res.status(200).json(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        response.error = true;
+
+        res.status(500).json(response);
+      });
+  },
+
+  LoginPlayer: async function (req, res) {
+
+    let response = {
+      error: false,
+      data: null,
+    };
+
+    const { email, password } = req.body;
+
+    await pool.query("SELECT * FROM players WHERE email = ?", email)
+      .then((result) => {
+
+        if (result.length === 0) {
+
+          response.error = true;
+          response.data = { msg: "check credential" }; //email error
+          res.status(401).json(response);
+
+        } else {
+
+            let row = null;
+            Object.keys(result).forEach((key) => {
+            row = result[key];
+          });
+
+          if (bcrypt.compareSync(password, row.password)) {
+            //if password ok return token
+            const token = jwt.sign(
+              { email: row.email, name: row.name },
+              authconfig.secret,
+              {
+                expiresIn: authconfig.expires,
+              }
+            );
+
+            response.data = { user: row.email, token: token };
+
+            res.status(200).json(response);
+          } else {
+            response.error = true;
+            response.data = { msg: "check credential" }; 
+            res.status(401).json(response);
+          }
+        }
+      })
+      .catch((error) => {
+        response.error = true;
+        response.data = error;
+
+        res.status(500).json(response);
+      });
+  },
+
+  
 };
--- backend/src/routes/playersRoutes.js
+++ backend/src/routes/playersRoutes.js
@@ -2,11 +2,33 @@ const express = require('express');
 const router = express.Router();
 const {Player} = require ('../controllers/playersController');
 
+
+
  router.get('/', async function (req,res) 
  {
-   await Player.GetDeck(req,res);
+   await Player.GetAllPlayers(req,res);
 
  });
+
+ router.get('/:id', async function (req,res) 
+ {
+   await Player.GetPlayer(req,res);
+
+ });
+
+ router.post('/', async function (req,res) 
+ {
+   await Player.RegisterPlayer(req,res);
+
+ });
+
+ router.post('/login', async function (req,res) 
+ {
+   await Player.LoginPlayer(req,res);
+
+ });
+
+
 
 
 module.exports= router;

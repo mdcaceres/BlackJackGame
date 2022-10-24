f7b809533c00eb4bd0a120494c2bef4e4d473503
mdcaceres <you@example.com>
2022-10-23 22:57:22 -0300

    add detail create func

--- frontend/src/app/services/detail.service.ts
+++ frontend/src/app/services/detail.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { detail } from '../interfaces/detail';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetailService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  create(detail:detail):Observable<detail>{
+    return this.http.post<detail>('....',detail); 
+  }
 }
